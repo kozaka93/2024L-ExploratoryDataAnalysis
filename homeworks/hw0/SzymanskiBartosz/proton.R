a8483308917943bc825a1f5afed45e56769b428a
Bartekk17 <160766439+Bartekk17@users.noreply.github.com>
2024-02-21 19:35:08 +0100

    add hw0

---
+++ homeworks/hw0/SzymanskiBartosz/proton.R
@@ -0,0 +1,10 @@
+install.packages("proton")
+library(proton)
+proton()
+
+
+employees[employees$surname == "Insecure",]
+for (i in 1:1000) {proton(action = "login", login="johnins", password=top1000passwords[i])}
+table(factor(logs[logs$login=="slap",]$host))
+
+
