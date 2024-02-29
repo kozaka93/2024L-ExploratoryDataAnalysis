4a0f6aae7ca065c288c1949e095f2f7d1cb318f2
bartolomeo3000 <133211311+bartolomeo3000@users.noreply.github.com>
2024-02-29 21:27:30 +0100

    Delete homeworks/hw0/bartoszpokora directory

--- homeworks/hw0/bartoszpokora/proton.R
+++
@@ -1,9 +0,0 @@
-install.packages("proton")
-library(proton)
-proton()
-
-employees <- employees
-login <- employees$login[c(employees$name, employees$surname) == c("John", "Insecure")]
-
-proton(action = "login", login= "johnins")
-
